7a4fbb638be17882a21428fa97db2b37a9b454b8
Raheem <raheemideadev@gmail.com>
2021-11-05 02:00:52 +0500

    initial

---
+++ src/main/kotlin/xyz/teamgravity/Application.kt
@@ -0,0 +1,14 @@
+package xyz.teamgravity
+
+import io.ktor.application.*
+import xyz.teamgravity.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
